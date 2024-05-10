c9e7c7ce54d4121e3088edf8e3eb30554b57bb54
Alex Ticlla <aticlla.09@gmail.com>
2024-05-10 14:54:00 -0400

    primer commit

---
+++ tictactoe.py
@@ -0,0 +1,168 @@
+"""
+Tic Tac Toe Player
+"""
+
+import math
+import copy
+
+X = "X"
+O = "O"
+EMPTY = None
+
+
+def initial_state():
+    """
+    Returns starting state of the board.
+    """
+    return [[EMPTY, EMPTY, EMPTY],
+            [EMPTY, EMPTY, EMPTY],
+            [EMPTY, EMPTY, EMPTY]]
+
+
+def player(board):
+    """
+    Returns player who has the next turn on a board.
+    """
+    count_x = 0
+    count_o = 0
+    for i in range (3):
+        for j in range (3):
+            if board[i][j] == X:
+                count_x += 1
+            elif board[i][j] == O:
+                count_o += 1
+    if count_x > count_o:
+        return O
+    else:
+        return X
+
+def actions(board):
+    """
+    Returns set of all possible actions (i, j) available on the board.
+    """
+    possible_actions = set()
+
+    for i in range(3):
+        for j in range(3):
+            if board[i][j] == EMPTY:
+                possible_actions.add((i, j))
+
+    return possible_actions
+
+
+def result(board, action):
+    """
+    Returns the board that results from making move (i, j) on the board.
+    """
+    # Create new board, without modifying the original board received as input
+    result = copy.deepcopy(board)
+    result[action[0]][action[1]] = player(board)
+    return result
+
+
+def winner(board):
+    """
+    Returns the winner of the game, if there is one.
+    """
+    # Check rows
+    for row in board:
+
+
+        if row.count(X) == 3:
+            return X
+        elif row.count(O) == 3:
+            return O
+
+    # Check columns
+    for col in range(3):
+        if board[0][col] == board[1][col] == board[2][col] == X:
+            return X
+        elif board[0][col] == board[1][col] == board[2][col] == O:
+            return O
+
+    # Check diagonals
+    if board[0][0] == board[1][1] == board[2][2] == X:
+        return X
+    elif board[0][0] == board[1][1] == board[2][2] == O:
+        return O
+    elif board[0][2] == board[1][1] == board[2][0] == X:
+        return X
+    elif board[0][2] == board[1][1] == board[2][0] == O:
+        return O
+
+    # No winner
+    return None
+
+def terminal(board):
+    """
+    Returns True if game is over, False otherwise.
+    """
+
+    if winner(board) is not None or (not any(EMPTY in sublist for sublist in board) and winner(board) is None):
+        return True
+    else:
+        return False
+    #return True if winner(board) is not None or (not any(EMPTY in sublist for sublist in board) and winner(board) is None) else False # noqa E501
+
+
+def utility(board):
+    """
+    Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
+    """
+    if terminal(board):
+        if winner(board) == X:
+            return 1
+        elif winner(board) == O:
+            return -1
+        else:
+            return 0
+    # Check how to handle exception when a non terminal board is received.
+
+
+def minimax(board):
+    """
+    Returns the optimal action for the current player on the board.
+    """
+    if terminal(board):
+        return None
+    else:
+        if player(board) == X:
+            value, move = max_value(board)
+            return move
+        else:
+            value, move = min_value(board)
+            return move
+
+def max_value(board):
+    if terminal(board):
+        return utility(board), None
+
+    v = float('-inf')
+    move = None
+    for action in actions(board):
+        # v = max(v, min_value(result(board, action)))
+        aux, act = min_value(result(board, action))
+        if aux > v:
+            v = aux
+            move = action
+            if v == 1:
+                return v, move
+
+    return v, move
+
+def min_value(board):
+    if terminal(board):
+        return utility(board), None
+
+    v = float('inf')
+    move = None
+    for action in actions(board):
+        # v = max(v, min_value(result(board, action)))
+        aux, act = max_value(result(board, action))
+        if aux < v:
+            v = aux
+            move = action
+            if v == -1:
+                return v, move
+            
+    return v, move
